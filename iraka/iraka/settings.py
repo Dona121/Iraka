495100d4fe61d3a03660acfad1f73f45780cf395
Dona121 <carrascal544@gmail.com>
2026-04-29 17:28:37 -0500

    Actualizado allowed host

--- iraka/iraka/settings.py
+++ iraka/iraka/settings.py
@@ -23,7 +23,6 @@ import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 
-
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 STATIC_URL = '/static/'
@@ -41,7 +40,8 @@ DEBUG = os.getenv("DEBUG")
 
 ALLOWED_HOSTS = os.environ.get(
     'ALLOWED_HOSTS', 
-    '127.0.0.1,localhost'
+    '127.0.0.1,localhost',
+    "iraka-production.up.railway.app"
 ).split(',')
 
 
