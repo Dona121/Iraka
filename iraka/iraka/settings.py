4c1c1aa5955d08e018361bfe8fb3cdcb19bd5750
Dona121 <carrascal544@gmail.com>
2026-04-29 17:39:26 -0500

    Ajuste en allowed host

--- iraka/iraka/settings.py
+++ iraka/iraka/settings.py
@@ -38,12 +38,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DEBUG")
 
-ALLOWED_HOSTS = os.environ.get(
-    'ALLOWED_HOSTS', 
-    '127.0.0.1,localhost',
-    "iraka-production.up.railway.app"
-).split(',')
-
+ALLOWED_HOSTS = ["*"]
 
 # Application definition
 
