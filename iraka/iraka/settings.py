8d4af89d7dd63cce1b111cbaffa92d41d7143be7
Dona121 <carrascal544@gmail.com>
2026-05-16 18:19:31 -0500

    Actualizada lista de dominios en ALLOWED_HOSTS

--- iraka/iraka/settings.py
+++ iraka/iraka/settings.py
@@ -38,7 +38,11 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DEBUG")
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = [
+        "*",
+        "irakahotelzenu.com",
+        "www.irakahotelzenu.com",
+    ]
 
 # Application definition
 
